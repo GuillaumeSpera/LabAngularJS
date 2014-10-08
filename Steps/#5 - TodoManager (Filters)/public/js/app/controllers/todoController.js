644e6bdea2291dcd4c30842c0107d875170878f7
Guillaume Spera <spera.guillaume@gmail.com>
2014-10-08 02:28:59 +0200

    Update previous steps. Add Step 5.

--- Steps/#5 - TodoManager (Filters)/public/js/app/controllers/todoController.js
+++ Steps/#5 - TodoManager (Filters)/public/js/app/controllers/todoController.js
@@ -5,12 +5,38 @@
 
     controllers.controller('todoController', function ($rootScope, $scope) {
         $scope.todoList = [];
+        $scope.todoPriority = {
+            low: 1,
+            medium: 2,
+            high: 3
+        };
+        $scope.priorityFilter = {};
+        $scope.sortOrder = 'id';
+        $scope.getPriorityName = getPriorityName;
 
         for (var i = 1; i < 11; i++) {
             $scope.todoList.push({
                 id: i,
-                description: 'Todo ' + i
+                description: 'Todo ' + i,
+                priority: $scope.todoPriority.high
             });
         }
+
+        function getPriorityName() {
+            switch ($scope.priorityFilter.priority) {
+                case $scope.todoPriority.low:
+                    return 'Low';
+                    break;
+                case $scope.todoPriority.medium:
+                    return 'Medium';
+                    break;
+                case $scope.todoPriority.high:
+                    return 'High';
+                    break;
+                default:
+                    return 'None';
+                    break;
+            }
+        };
     });
 })();
